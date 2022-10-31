8e4a48e273a64c88d25cc3409c115f8e06828985
HoanggSoGOOD <112925959+HoanggSoGOOD@users.noreply.github.com>
2022-10-31 12:40:22 +0700

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -1,21 +1,21 @@
 @echo off
-del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By LittleFox" > out.txt 2>&1
+del /f "C:\Users\Public\Desktop\UnityHub.ink" > out.txt 2>&1
+net config server /srvcomment:"Windows 10 Pro" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user LittleFox 6454@6454 /add >nul
-net localgroup administrators LittleFox /add >nul
+net user Mandu 123 /add >nul
+net localgroup administrators Mandu /add >nul
 net user LittleFox /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
-sc config Audiosrv start= auto >nul
+sc config Audiosrv start= off >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant LittleFox:F >nul
-ICACLS C:\Windows\installer /grant LittleFox:F >nul
+ICACLS C:\Windows\Temp /grant Mandu:F >nul
+ICACLS C:\Windows\installer /grant Mandu:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: LittleFox
-echo Password: 6454@6454
+echo Username: Mandu
+echo Password: 123
 echo You can login now!
 ping -n 10 127.0.0.1 >nul
